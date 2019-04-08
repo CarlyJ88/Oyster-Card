d3ae1ad0c80cf2ae91c1db53247403425798485f
Carly <carly.jenkinson65@googlemail.com>
2019-04-08 15:30:09 +0100

    Test for top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,10 @@ describe Oystercard do
   
   it 'responds to balance' do
     expect(subject).to respond_to :balance
+
+    it 'responds to top up'
+    expect(subject).to respond_to :top_up
+    
   end
 
 
